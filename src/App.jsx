a99e2577dac9f3af1287fe5a2c681f5098fd43a3
burcaaurelia-art <burcaaurelia@gmail.com>
2025-11-13 18:41:03 +0200

    Update App.jsx
    add cere-oferta route

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 import BlogList from "./components/BlogList.jsx";
 import BlogPost from "./components/BlogPost.jsx";
+import CereOferta from "./CereOferta.jsx";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-black text-white flex flex-col">
       <Navbar />
       <main className="pt-[120px] sm:pt-[140px] px-4 flex-1">
-        <Routes>
-          <Route path="/" element={<BlogList />} />
-          <Route path="/blog/:id" element={<BlogPost />} />
-        </Routes>
+<Routes>
+  <Route path="/" element={<BlogList />} />
+  <Route path="/blog/:id" element={<BlogPost />} />
+  <Route path="/cere-oferta" element={<CereOferta />} /> {/* 👈 nou */}
+</Routes>
       </main>
       <Footer />
     </div>

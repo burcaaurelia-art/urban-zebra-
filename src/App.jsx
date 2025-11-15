e316f0abbcc39e5d8ef0e0003c25878d4005a769
burcaaurelia-art <burcaaurelia@gmail.com>
2025-11-15 14:09:25 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 import BlogList from "./components/BlogList.jsx";
@@ -10,13 +11,15 @@ export default function App() {
   return (
     <div className="min-h-screen bg-black text-white flex flex-col">
       <Navbar />
-      <main className="pt-[120px] sm:pt-[140px] px-4 flex-1">
-<Routes>
-  <Route path="/" element={<BlogList />} />
-  <Route path="/blog/:id" element={<BlogPost />} />
-  <Route path="/cere-oferta" element={<CereOferta />} /> {/* 👈 nou */}
-</Routes>
+
+      <main className="pt-[120px] px-4 flex-1 transition-all duration-300">
+        <Routes>
+          <Route path="/" element={<BlogList />} />
+          <Route path="/blog/:id" element={<BlogPost />} />
+          <Route path="/cere-oferta" element={<CereOferta />} />
+        </Routes>
       </main>
+
       <Footer />
     </div>
   );

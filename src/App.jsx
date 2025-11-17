cf6962eb1698ea6996296eb6d073f57c0792374b
burcaaurelia-art <burcaaurelia@gmail.com>
2025-11-17 15:59:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx";
-import BlogList from "./components/BlogList.jsx";
-import BlogPost from "./components/BlogPost.jsx";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import BlogList from "./pages/BlogList";
+import BlogPost from "./pages/BlogPost";
+
+// Import corect pentru CereOferta
 import CereOferta from "./CereOferta.jsx";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-black text-white flex flex-col">
+      {/* Navbar */}
       <Navbar />
 
-      <main className="pt-[120px] px-4 flex-1 transition-all duration-300">
+      {/* Conținut */}
+      <main className="pt-[120px] sm:pt-[140px] px-4 flex-1 transition-all duration-300">
         <Routes>
           <Route path="/" element={<BlogList />} />
           <Route path="/blog/:id" element={<BlogPost />} />
@@ -20,6 +25,7 @@ export default function App() {
         </Routes>
       </main>
 
+      {/* Footer */}
       <Footer />
     </div>
   );
